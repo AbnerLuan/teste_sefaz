aca8567549ca8306d1eb45c623f1bedc60ea3ac7
Abner Luan <abnerldsantos@gmail.com>
2022-03-20 19:12:32 -0300

    Implementação do Cadastro de Endereços
    - Adicionado método para cadastro de endereço com base no CEP, através de um webservice disponibilizado no site "viacep".
    -Melhoria no front-end (aparencia)

--- src/main/java/br/com/sefaz/bean/UsuarioBean.java
+++ src/main/java/br/com/sefaz/bean/UsuarioBean.java
@@ -1,11 +1,21 @@
 package br.com.sefaz.bean;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.Serializable;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.event.AjaxBehaviorEvent;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+
+import com.google.gson.Gson;
 
 import br.com.sefaz.dao.Dao;
 import br.com.sefaz.model.Usuario;
@@ -23,6 +33,34 @@ public class UsuarioBean implements Serializable {
 	private List<Usuario> list = new ArrayList<Usuario>();
 
 	private IDaoUsuario iDaoUsuario = new IDaoUsuarioImpl();
+
+	public void pesquisaCep(AjaxBehaviorEvent event) {
+		try {
+
+			URL url = new URL("https://viacep.com.br/ws/" + usuario.getCep() + "/json/");
+			URLConnection connection = url.openConnection();
+			InputStream is = connection.getInputStream();
+			BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+
+			String cep = "";
+			StringBuilder jsonCep = new StringBuilder();
+
+			while ((cep = br.readLine()) != null) {
+				jsonCep.append(cep);
+			}
+			Usuario userCep = new Gson().fromJson(jsonCep.toString(), Usuario.class);
+
+			usuario.setCep(userCep.getCep());
+			usuario.setLogradouro(userCep.getLogradouro());
+			usuario.setLocalidade(userCep.getLocalidade());
+			usuario.setBairro(userCep.getBairro());
+			usuario.setUf(userCep.getUf());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
 
 	public String salvar() {
 		dao.salvar(usuario);
@@ -65,7 +103,12 @@ public class UsuarioBean implements Serializable {
 			return "cadastro.xhtml";
 		}
 
-		return "index.xhtml";
+		usuario = new Usuario();
+	      FacesContext.getCurrentInstance().addMessage(
+	         null,
+	         new FacesMessage(FacesMessage.SEVERITY_ERROR, "Usuário não encontrado!",
+	           "Erro no Login!"));
+	      return null;
 	}
 
 }
--- src/main/java/br/com/sefaz/model/Usuario.java
+++ src/main/java/br/com/sefaz/model/Usuario.java
@@ -24,6 +24,57 @@ public class Usuario implements Serializable {
 	private String email;
 
 	private String senha;
+	
+	private String cep;
+	
+	private String logradouro;
+	
+	private String bairro;
+	
+	private String localidade;
+	
+	private String uf;
+	
+	
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getLocalidade() {
+		return localidade;
+	}
+
+	public void setLocalidade(String localidade) {
+		this.localidade = localidade;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
 
 	@OneToMany(mappedBy = "usuario", fetch = FetchType.EAGER)
 	private List<TelefoneUser> telefoneUsers;
